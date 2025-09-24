c1f38a35773288fb2c8895b465e3992a030683c7
7flowsstudio <seven.flows.studio@gmail.com>
2025-09-24 12:10:23 +0200

    add first commit

---
+++ src/app/layout.tsx
@@ -0,0 +1,28 @@
+import type { Metadata } from "next";
+import "./globals.css";
+import Header from "@/components/Header/Header";
+import Footer from "@/components/Footer/Footer";
+
+export const metadata: Metadata = {
+	title: "Octopus-hair",
+	description: "Najlepszy zakład fryzjerski w Piotrkowie-Tribunalskoje",
+	icons: {
+		icon: "/favicon.ico",
+	},
+};
+
+export default function RootLayout({
+	children,
+}: Readonly<{
+	children: React.ReactNode;
+}>) {
+	return (
+		<html lang="pl">
+			<body>
+				<Header />
+				<main>{children}</main>
+				<Footer />
+			</body>
+		</html>
+	);
+}
---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Header = () => {
+	return <div>Hader</div>;
+};
+
+export default Header;
