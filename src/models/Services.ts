ce626ea00eb1af4451c817e6ece31ed00d8ffc8b
7flowsstudio <seven.flows.studio@gmail.com>
2025-09-27 11:04:26 +0200

    Revert "Initial commit: backend structure with routes"
    This reverts commit 50701f87edac5ba55d5d85665c435f3ed94955b7.

--- src/models/Services.ts
+++
@@ -1,9 +0,0 @@
-import { request } from "http";
-import mongoose, { Schema, model, models } from "mongoose";
-const serviceSchema = new mongoose.Schema({
-  name: { type: String, required: true },
-  duration: { type: Number, required: true },
-  price: { type: Number, required: true },
-});
-
-const Service = mongoose.model("Service", serviceSchema);
--- src/routes/bookings.ts
+++
@@ -1,7 +0,0 @@
-import { Router, Request, Response } from "express";
-const router = Router();
-router.delete("/:id", (req: Request, res: Response) => {
-  res.json({ message: "Deleted" });
-});
-
-export default router;
