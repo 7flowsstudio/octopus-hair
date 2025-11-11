7f36806df1f66baf9fe38961614bfde3ebb53e03
VitaliiBarabash1990 <alievruslan1818@gmail.com>
2025-11-11 16:44:29 +0100

    add variabless

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import "./globals.css";
+import "./variables.css";
 import Header from "@/components/Header/Header";
 import Footer from "@/components/Footer/Footer";
 
