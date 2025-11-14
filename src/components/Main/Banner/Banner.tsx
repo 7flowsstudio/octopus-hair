cf18a77882fb1d5f9c14e83900bb872d9dee0e5a
ABurianenko <121970095+ABurianenko@users.noreply.github.com>
2025-11-14 00:16:56 +0000

    Add promotional Banner component to homepage
    Introduces a new Banner component with styles and images to highlight a 'tenth haircut free' promotion. The Banner is added to the homepage and includes a background image and SVG icon.

---
+++ src/components/Main/Banner/Banner.tsx
@@ -0,0 +1,18 @@
+import Image from "next/image";
+import s from './Banner.module.css'
+
+export const Banner = () => {
+    return (
+        <section className={`container ${s.banner}`}>
+            <h2 className={s.title}>
+                CO DZIESIĄTE STRZYŻENIE GRATIS!
+            </h2>
+            <Image
+                src="/images/Banner/Gift.svg"
+                alt="prezent"
+                width={75}
+                height={78}
+            />
+        </section>
+    )
+}
